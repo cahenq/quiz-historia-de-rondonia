8c79602d06ffa2a6d33c9092bd98c0d1f9f31bb6
cahenq <cauahenriqu000@gmail.com>
2023-10-08 14:32:22 -0400

    lista de perguntas

--- lib/tela_perguntas.dart
+++ lib/tela_perguntas.dart
@@ -1,43 +1,98 @@
 import 'package:flutter/material.dart';
-import 'tela_inicio.dart'; // Importe o arquivo tela_inicio.dart
+import 'tela_inicio.dart';
 
-class TelaPerguntas extends StatelessWidget {
+class TelaPerguntas extends StatefulWidget {
+  @override
+  _TelaPerguntasState createState() => _TelaPerguntasState();
+}
+
+class _TelaPerguntasState extends State<TelaPerguntas> {
+  int _currentQuestionIndex = 0;
+
+  List<Map<String, dynamic>> perguntas = [
+    {
+      'pergunta': 'Qual é a capital do Brasil?',
+      'alternativas': ['São Paulo', 'Rio de Janeiro', 'Brasília', 'Salvador'],
+      'resposta': 'Brasília',
+    },
+    {
+      'pergunta': 'Quantos planetas existem no sistema solar?',
+      'alternativas': ['Sete', 'Oito', 'Nove', 'Dez'],
+      'resposta': 'Oito',
+    },
+    {
+      'pergunta': 'Quem escreveu Dom Quixote?',
+      'alternativas': ['Machado de Assis', 'Cervantes', 'Shakespeare', 'Dostoiévski'],
+      'resposta': 'Cervantes',
+    },
+    // Adicione mais perguntas conforme necessário
+  ];
+
+  void _responderPergunta(String resposta) {
+    if (resposta.toLowerCase() == perguntas[_currentQuestionIndex]['resposta'].toLowerCase()) {
+      if (_currentQuestionIndex < perguntas.length - 1) {
+        setState(() {
+          _currentQuestionIndex++;
+        });
+      } else {
+        print('Todas as perguntas foram respondidas');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tela de Perguntas'),
-      ),
-      body: Center(
+      appBar: null,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xFF002081), Color(0xFF068946)],
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Pergunta 1',
-              style: TextStyle(fontSize: 24),
+              perguntas[_currentQuestionIndex]['pergunta'],
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TelaInicio()),
+            SizedBox(height: 20.0),
+            Column(
+              children: (perguntas[_currentQuestionIndex]['alternativas'] as List<String>).map((alternativa) {
+                return SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => _responderPergunta(alternativa),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                      child: Text(
+                        alternativa,
+                        style: TextStyle(
+                          fontFamily: 'GeometryExtraBold',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
                 );
-              },
-              child: Text('Resposta 1'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Pergunta 2',
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TelaInicio()),
-                );
-              },
-              child: Text('Resposta 2'),
+              }).toList(),
             ),
           ],
         ),
